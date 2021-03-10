6327caf34c449a7c55069c4b189fb35c29653a14
Lucas Czarnecki <62263095+Lucas-Czarnecki@users.noreply.github.com>
2021-03-09 19:14:16 -0700

    Add Number of Parties to Election-level Results
    This commit adds the number of parties by election. The commit also pushes a cleaned script and removes dates from combined keys.

--- R/LOP_RidingsSummaries.R
+++ R/LOP_RidingsSummaries.R
@@ -15,11 +15,8 @@ FED_1867_present <- readRDS("~/GitHub/Canadian-Federal-Elections/data/cleaned/ma
 
 # ---- Data Processing ----
 
-# TODO
-# Rename List_Ridings to indicate data are a constituency-level summary.
-
-# create the same combined key for master data set.
-FED_1867_present$Combined_Key <- paste(as.character(FED_1867_present$Election_Date), FED_1867_present$Constituency, FED_1867_present$Province_Territory, sep = ", ")
+# Create a combined key that can distinguish between distinct constituencies with identical names but located in different provinces.
+FED_1867_present$Combined_Key <- paste(FED_1867_present$Constituency, FED_1867_present$Province_Territory, sep = ", ")
 
 # 1. Create a summary table of constituency-level results.
 Summary_of_Ridings <- FED_1867_present %>% 
@@ -33,6 +30,17 @@ Summary_of_Elections <- Summary_of_Ridings %>%
   group_by(Election_Date, Election_Type) %>% 
   summarise(Number_of_Ridings = length(unique(Combined_Key)), Number_of_Seats = sum(District_Magnitude), Number_of_Candidates = sum(Number_of_Candidates), Total_Valid_Votes = sum(Valid_Votes), .groups='drop')
 
+# Obtain number of political parties by election. 
+Summary_of_Parties <- FED_1867_present %>% 
+  group_by(Election_Date, Election_Type, Parliament) %>% 
+  summarise(Number_of_Parties = length(unique(Political_Affiliation)), .groups='drop')
+
+# Match the number of parties to election-level results.
+Summary_of_Elections$Number_of_Parties <- Summary_of_Parties$Number_of_Parties[match(Summary_of_Elections$Election_Date, Summary_of_Parties$Election_Date)]
+
+# Select preferred column order.
+Summary_of_Elections <- Summary_of_Elections %>% 
+  select(Election_Date, Election_Type, Number_of_Ridings, Number_of_Seats, Number_of_Candidates, Number_of_Parties, Total_Valid_Votes)
 
 # ---- Export Data ----
 
