4256a54c28ddf6ca5dec474ab7dfed7b265a1036
Lucas Czarnecki <lczarnec@alumni.ucalgary.ca>
2026-01-15 14:59:24 -0700

    Create new python workflow

---
+++ Py/01_fetch_parlinfo.py
@@ -0,0 +1,14 @@
+#%%
+# jupytext:
+#   formats: ipynb,py:percent
+#   notebook_metadata_filter: kernelspec
+#   text_representation:
+#     extension: .py
+#     format_name: percent
+#     format_version: '1.3'
+#     jupytext_version: 1.16.0
+# kernelspec:
+#   display_name: Python 3
+#   language: python
+#   name: python3
+# ---
